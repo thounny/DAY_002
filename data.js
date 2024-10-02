53e8c24291aaa888672a8d441229fd8237136a46
Thounny <thounnykeo@gmail.com>
2024-10-02 15:10:01 -0400

    Initial commit | messed up first repo

---
+++ data.js
@@ -0,0 +1,42 @@
+export const slides = [
+  {
+    title: "JIN JIA JI",
+    id: "TK82734",
+  },
+  {
+    title: "Nike football",
+    id: "TK19305",
+  },
+  {
+    title: "Editorial",
+    id: "TK94721",
+  },
+  {
+    title: "Salomon",
+    id: "TK56389",
+  },
+  {
+    title: "NIKE",
+    id: "TK28417",
+  },
+  {
+    title: "LOEWE",
+    id: "TK75931",
+  },
+  {
+    title: "Vuum",
+    id: "TK69284",
+  },
+  {
+    title: "Creative Loop",
+    id: "TK34825",
+  },
+  {
+    title: "GOAT",
+    id: "TK17369",
+  },
+  {
+    title: "Reflect Studio",
+    id: "TK59382",
+  },
+];
